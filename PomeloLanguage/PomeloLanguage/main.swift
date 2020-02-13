d61fbcd3c51de6e6620cea462fda9b373ebe1181
徐仲平 <1021057927@qq.com>
2020-02-13 14:48:19 +0800

    use swift

--- PomeloLanguage/PomeloLanguage/main.swift
+++ PomeloLanguage/PomeloLanguage/main.swift
@@ -1,9 +1,12 @@
 //
-//  PoLangObject.cpp
+//  main.swift
 //  PomeloLanguage
 //
 //  Created by 徐仲平 on 2020/2/13.
 //  Copyright © 2020 徐仲平. All rights reserved.
 //
 
-#include "PLObject.hpp"
+import Foundation
+
+print("Hello, World!")
+
